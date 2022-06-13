e45da5aaa71366af8705b49f66a92c0fc93b1999
flylee <flyleekungfu@163.com>
2022-06-14 00:50:47 +0800

    项目初始化

---
+++ gulimall-coupon/src/test/java/com/flylee/gulimall/coupon/GulimallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flylee.gulimall.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
