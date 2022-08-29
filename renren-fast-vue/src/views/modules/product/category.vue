4a3341cf99cbca2b5b666421a3977fdd34a5550f
flylee <flyleekungfu@163.com>
2022-08-29 14:19:11 +0800

    谷粒商城前端项目提交

---
+++ renren-fast-vue/src/views/modules/product/category.vue
@@ -0,0 +1,86 @@
+<template>
+  <el-tree :data="menus"
+           show-checkbox
+           node-key="catId"
+           default-expand-all
+           :expand-on-click-node="false"
+           :props="defaultProps">
+    <span class="custom-tree-node" slot-scope="{ node, data }">
+        <span>{{ node.label }}</span>
+        <span>
+          <el-button
+            v-if="node.level <= 2"
+            type="text"
+            size="mini"
+            @click="() => append(data)">
+            Append
+          </el-button>
+          <el-button
+            v-if="node.childNodes.length === 0"
+            type="text"
+            size="mini"
+            @click="() => remove(node, data)">
+            Delete
+          </el-button>
+        </span>
+      </span>
+  </el-tree>
+</template>
+
+<script>
+export default {
+  name: 'category',
+  data () {
+    return {
+      menus: [],
+      defaultProps: {
+        children: 'children',
+        label: 'name'
+      }
+    }
+  },
+  created () {
+    this.getMenus()
+  },
+  methods: {
+    getMenus () {
+      this.$http({
+        url: this.$http.adornUrl('/product/category/list/tree'),
+        method: 'get'
+      }).then(({data}) => {
+        this.menus = data
+      })
+    },
+    append (data) {
+      console.log('append', data)
+    },
+    remove (node, data) {
+      console.log('remove', node, data)
+      var ids = [data.catId]
+      this.$confirm(`是否删除【${data.name}】菜单`, '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        this.$http({
+          url: this.$http.adornUrl('/product/category/delete'),
+          method: 'post',
+          data: this.$http.adornData(ids, false)
+        }).then(({data}) => {
+          this.$message({
+            message: '菜单删除成功',
+            type: 'success'
+          })
+          // 刷新菜单
+          this.getMenus()
+        })
+      }).catch(() => {
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
