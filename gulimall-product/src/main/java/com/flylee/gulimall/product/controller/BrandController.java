5ebf660442037d5ad70ce73c7b6aecb64d2881e1
flylee <flyleekungfu@163.com>
2022-12-09 16:24:25 +0800

    OSS上传

--- gulimall-product/src/main/java/com/flylee/gulimall/product/controller/BrandController.java
+++ gulimall-product/src/main/java/com/flylee/gulimall/product/controller/BrandController.java
@@ -1,7 +1,5 @@
 package com.flylee.gulimall.product.controller;
 
-import com.aliyun.oss.OSSClient;
-import com.aliyun.oss.OSSException;
 import com.flylee.gulimall.common.utils.PageUtils;
 import com.flylee.gulimall.common.utils.R;
 import com.flylee.gulimall.product.entity.BrandEntity;
@@ -9,9 +7,6 @@ import com.flylee.gulimall.product.service.BrandService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.InputStream;
 import java.util.Arrays;
 import java.util.Map;
 
@@ -29,8 +24,6 @@ import java.util.Map;
 public class BrandController {
     @Autowired
     private BrandService brandService;
-    @Autowired
-    private OSSClient ossClient;
 
     /**
      * 列表
@@ -79,37 +72,6 @@ public class BrandController {
     @RequestMapping("/delete")
     public R delete(@RequestBody Long[] brandIds){
 		brandService.removeByIds(Arrays.asList(brandIds));
-
-        return R.ok();
-    }
-
-    @RequestMapping("/test")
-    public R test() throws FileNotFoundException {
-
-        // 填写Bucket名称，例如examplebucket。
-        String bucketName = "gulimall-flylee";
-        // 填写Object完整路径，完整路径中不能包含Bucket名称，例如exampledir/exampleobject.txt。
-        String objectName = "exampledir/exampleobject.txt";
-        // 填写本地文件的完整路径，例如D:\\localpath\\examplefile.txt。
-        // 如果未指定本地路径，则默认从示例程序所属项目对应本地路径中上传文件流。
-        String filePath= "D:\\test\\test.txt";
-
-        try {
-            InputStream inputStream = new FileInputStream(filePath);
-            // 创建PutObject请求。
-            ossClient.putObject(bucketName, objectName, inputStream);
-        } catch (OSSException oe) {
-            System.out.println("Caught an OSSException, which means your request made it to OSS, "
-                    + "but was rejected with an error response for some reason.");
-            System.out.println("Error Message:" + oe.getErrorMessage());
-            System.out.println("Error Code:" + oe.getErrorCode());
-            System.out.println("Request ID:" + oe.getRequestId());
-            System.out.println("Host ID:" + oe.getHostId());
-        } finally {
-            if (ossClient != null) {
-                ossClient.shutdown();
-            }
-        }
 
         return R.ok();
     }
